a494fcf43e206157ea24f95b53ff0d76dce072eb
Teona Maghradze <maghradzeteo@gmail.com>
2023-12-12 13:15:23 +0400

    add intial files

---
+++ e-commerce-app/src/App.js
@@ -0,0 +1,47 @@
+import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
+import Products from "./pages/Products/Products";
+import Home from "./pages/Home/Home";
+import Product from "./pages/Product/Product";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
---
+++ e-commerce-app/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,66 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronDown,
+  faHeart,
+  faShoppingCart,
+} from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left">
+          <div className="item">
+            <img style={{ width: "20px" }} src="/flag.png" alt="" />
+            <FontAwesomeIcon icon={faChevronDown} />
+          </div>
+          <div className="item">
+            <span>USD</span>
+            <FontAwesomeIcon icon={faChevronDown} />
+          </div>
+
+          <div className="item">
+            <Link to="/products/1">Women</Link>
+            <Link to="/products/2">Men</Link>
+            <Link to="/products/3">Children</Link>
+          </div>
+        </div>
+        <div className="center">
+          <Link to="/">ONLINE STORE</Link>
+        </div>
+
+        <div className="right">
+          <div className="item">
+            <Link to="/">Homepage</Link>
+          </div>
+          <div className="item">
+            <Link to="/">About</Link>
+          </div>
+          <div className="item">
+            <Link to="/">Contact</Link>
+          </div>
+          <div className="item">
+            <Link to="/">Stores</Link>
+          </div>
+
+          <div className="icons">
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+            <FontAwesomeIcon icon={faUser} />
+            <FontAwesomeIcon icon={faHeart} />
+            <div className="cart-icon">
+              <FontAwesomeIcon icon={faShoppingCart} />
+              <span>0</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
